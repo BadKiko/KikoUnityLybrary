777f699696db50406de391e15b751d239b79d3fc
Kiko <kirill.kif234@gmail.com>
2021-09-27 18:18:38 +0300

    StartMyLibrary

---
+++ Unity-UI-Rounded-Corners/UiRoundedCorners/ImageWithRoundedCorners.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ImageWithRoundedCorners : MonoBehaviour {
+	private static readonly int Props = Shader.PropertyToID("_WidthHeightRadius");
+
+	public Material material;
+	[SerializeField]
+	private float radius = 150;
+	[Range(0,50)]
+	public float coeff = 10;
+
+	void OnRectTransformDimensionsChange(){
+		Refresh();
+	}
+	
+	private void OnValidate(){
+		Refresh();
+	}
+
+	[ContextMenu("Refresh")]
+    private void Refresh(){
+        radius = Screen.width / coeff - 10;
+        var rect = ((RectTransform) transform).rect;
+		material.SetVector(Props, new Vector4(rect.width, rect.height, radius, 0));
+	}
+}
